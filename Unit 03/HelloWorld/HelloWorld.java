3e9dd24668d945474f5d0bbe17c39b1c329d3d57
IL0VERAT <26mlinnboggs@derryfield.org>
2023-10-02 19:49:30 -0400

    Added HelloWorld code

---
+++ Unit 03/HelloWorld/HelloWorld.java
@@ -0,0 +1,6 @@
+//Coder: Milo Linn-Boggs Date: 02 Octo. 2023
+public class HelloWorld{
+    public static void main (String[]args) throws Exception{
+        System.out.print("Hello World: The Milo Version");//prints statement
+    }
+}
