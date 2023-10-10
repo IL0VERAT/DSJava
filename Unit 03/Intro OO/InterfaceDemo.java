93a6d8cf5ac32345b68c1958eb473b7992b7d06a
IL0VERAT <26mlinnboggs@derryfield.org>
2023-10-10 11:16:08 -0400

    added some comments

--- Unit 03/Intro OO/InterfaceDemo.java
+++ Unit 03/Intro OO/InterfaceDemo.java
@@ -1,5 +1,6 @@
-interface Wallet {
-    public int getCards();
+//Coder: Milo Linn-Boggs Date: 10 October 2023
+interface Wallet { 
+    public int getCards();//Lines 3-10: What the new class will implement 
     public int getBills();
 
     public void plusCards();
@@ -9,38 +10,38 @@ interface Wallet {
     public void minusBills();
 }
 
-class OnlineWallet implements Wallet{
-    int cards = 0;
+class OnlineWallet implements Wallet{ //Actuall implementation
+    int cards = 0; //Sets values
     int bills = 0;
     int credits = 0;
 
-    public int getCards(){
+    public int getCards(){ //Returns # of cards
         return cards;
     }
 
-    public int getBills(){
+    public int getBills(){ //Returns # of bills
         return bills;
     }
-    public int getCredits(){
+    public int getCredits(){ // Returns # of credits
         return credits;
     }
 
-    public void plusCards(){
+    public void plusCards(){ //Adds 1 card and 1 credit
         cards = cards + 1;
         credits = credits + 1;
     }
 
-    public void minusCards(){
+    public void minusCards(){ //Subtracts 1 card if possible
         if (cards > 0){
             cards = cards - 1;}
     }
 
-    public void plusBills(){
+    public void plusBills(){ //Addeds 1 bill and 1 credit
         bills = bills + 1;
         credits = credits + 1;
     }
 
-    public void minusBills(){
+    public void minusBills(){ //Subtracts 1 bill. 
        if (bills > 0) {
          bills = bills - 1;}
     }
@@ -51,14 +52,14 @@ class OnlineWallet implements Wallet{
 
 public class InterfaceDemo{
     public static void main (String[]args){
-        OnlineWallet ewallet = new OnlineWallet();
-        System.out.printf("Number of cards is %d%nNumber of bills is %d%nNumber of credits is %d%n", ewallet.getCards(), ewallet.getBills(), ewallet.getCredits());
+        OnlineWallet ewallet = new OnlineWallet(); //installs the new OnlineWallet class
+        System.out.printf("Number of cards is %d%nNumber of bills is %d%nNumber of credits is %d%n", ewallet.getCards(), ewallet.getBills(), ewallet.getCredits()); //Displays current state of Online Wallet
         
-        System.out.printf("Now lets add a bill and a card to your Online Wallet.%n");
-        ewallet.plusBills();
+        System.out.printf("Now lets add a bill and a card to your Online Wallet.%n"); //Gives example of some functions for the Online Wallet
+        ewallet.plusBills(); //Lines 59-60: Code needed to show the example
         ewallet.plusCards();
         
-        System.out.printf("Number of cards is %d%nNumber of bills is %d%nNumber of credits is %d%n", ewallet.getCards(), ewallet.getBills(), ewallet.getCredits());
-        System.out.println("Look, you now have two credits that you got by added a card and a bill to your Online Wallet");
+        System.out.printf("Number of cards is %d%nNumber of bills is %d%nNumber of credits is %d%n", ewallet.getCards(), ewallet.getBills(), ewallet.getCredits()); //Displays the new state of Online Wallet
+        System.out.println("Look, you now have two credits that you got by added a card and a bill to your Online Wallet"); //Explains to use the credit system
     }
 }
--- Unit 03/Intro OO/WalletOODemo.java
+++ Unit 03/Intro OO/WalletOODemo.java
@@ -1,37 +1,38 @@
-class Wallet{
-    final int MAX_CARDS = 15;
+//Coder: Milo Linn-Boggs Date: 10 October 2023
+class Wallet{ //class created
+    final int MAX_CARDS = 15; //Lines 3-6: Details about the class 
     final int MAX_BILLS = 25;
     int cards = 0;
     int bills = 0;
 
-    public int getCards(){
+    public int getCards(){ //Returns the # of cards
         return cards;
     }
-    public int getBills(){
+    public int getBills(){ //Returns the # of bills
         return bills;
     }
 
-    public void plusCards(){
+    public void plusCards(){ //Adds 1 card to wallet if possible
         //increases # of cards if possible
         if (cards < MAX_CARDS){
             cards = cards + 1;
         }
     }
 
-    public void minusCards(){
+    public void minusCards(){ //subtracts 1 card from wallet if possible
         if (cards > 0){
             cards = cards - 1;
         }
 
     }
 
-    public void plusBills(){
+    public void plusBills(){ //Adds 1 bill to wallet if possible
         if (bills < MAX_BILLS){
             bills = bills + 1;
         }
     }
 
-    public void minusBills(){
+    public void minusBills(){ // Subtracts 1 bill from wallet if possible
         if (bills > 0){
             bills = bills - 1;
         }
@@ -39,13 +40,13 @@ class Wallet{
 
 
 }
-public class WalletOODemo {
+public class WalletOODemo { //Program class
     public static void main (String[]args){
-        Wallet wallet = new Wallet();
-        System.out.printf("The number of cards in your wallet is %d and the number of bills is %d" ,wallet.getCards(), wallet.getBills());
-        System.out.printf("%nNow the computer will add 1 card and 1 bill to the wallet.");
-        wallet.plusCards();
+        Wallet wallet = new Wallet(); //implements the class created
+        System.out.printf("The number of cards in your wallet is %d and the number of bills is %d" ,wallet.getCards(), wallet.getBills()); //Displays wallets current status to user
+        System.out.printf("%nNow the computer will add 1 card and 1 bill to the wallet."); //Example of what program can do
+        wallet.plusCards(); //Lines 48-49: Code needed to add 1 card and 1 bill
         wallet.plusBills();
-        System.out.printf("%nYour wallet now has %d cards and %d bills.", wallet.getCards(), wallet.getBills());
+        System.out.printf("%nYour wallet now has %d cards and %d bills.", wallet.getCards(), wallet.getBills()); //Shows user result
     }
 }
