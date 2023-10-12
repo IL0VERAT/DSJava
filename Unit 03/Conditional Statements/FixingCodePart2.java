050d5c252803e375ce373652e9233451b8067e9c
IL0VERAT <26mlinnboggs@derryfield.org>
2023-10-11 20:58:49 -0400

    Created FixingCodePart2
    Talk to Mr. Eng on this code. Something is not working correctly.

---
+++ Unit 03/Conditional Statements/FixingCodePart2.java
@@ -0,0 +1,13 @@
+public class FixingCodePart2 {
+    public static void main (String[]args) throws Exception{
+        final double THRESHOLD = 1e-45;
+        double a = 1.2; 
+        double b= 3.0;
+        double c = a * b; 
+        if(Math.abs(c - 3.6) < THRESHOLD){
+        System.out.println("c is 3.6");
+        } else {
+        System.out.println("c is not 3.6");
+        }
+    }
+}
