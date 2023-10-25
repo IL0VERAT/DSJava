f4a663e3046d8375eb1727118182e490fea2ac5d
IL0VERAT <26mlinnboggs@derryfield.org>
2023-10-24 22:31:23 -0400

    Created InterestRate.java

---
+++ Unit 04/Intro to Loops/InterestRate.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class InterestRate {
+    public static void main(String[]args){
+    Scanner in = new Scanner(System.in);
+    System.out.print("What is the interest rate (APR)? Please input as a decimal value: ");
+    double interest_rate = in.nextDouble();
+    System.out.print("What is the initial balance amount in dollars: ");
+    double initial_balance = in.nextDouble();
+    double result;
+    for (int y = 1; y < 6; y++){
+        result = initial_balance + (initial_balance * 0.05);
+        System.out.printf("")
+    }
+    }
+    }
+    
+
