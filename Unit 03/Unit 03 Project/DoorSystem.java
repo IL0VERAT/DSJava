f3c681f11059cc9a837ed5e16e8510719b13b156
IL0VERAT <26mlinnboggs@derryfield.org>
2023-10-25 20:27:48 -0400

    Work on 03 Project

--- Unit 03/Unit 03 Project/DoorSystem.java
+++ Unit 03/Unit 03 Project/DoorSystem.java
@@ -1,21 +1,128 @@
 import java.util.Scanner;
 class DoorSystem {
         enum GearShift { P, N, D, ONE, TWO, THREE, R }
-        boolean Dash_Switch_Left; 
-        boolean Dash_Switch_Right;
-        boolean Child_Lock;
-        boolean Master_Unlock;
-        boolean Outside_Handle_Left;
-        boolean Outside_Handle_Right;
-        boolean Inside_Handle_Left;
-        boolean Inside_Handle_Right;
+        //Note to self: eventually just make one string and use substrings for methods + fix capitalzation 
+        String Dash_Switch_Left; 
+        String Dash_Switch_Right;
+        String Child_Lock;
+        String Master_Unlock;
+        String Outside_Handle_Left;
+        String Outside_Handle_Right;
+        String Inside_Handle_Left;
+        String Inside_Handle_Right;
         Gearshift Gear;
-
-        public void Dash_SwitchL_Status(){
-            if(Dash_Switch_Left = true){
-                
+        
+        
+        boolean left_door_dashswitch;
+        public void Dash_SwitchL_Code(){
+            if(Dash_Switch_Left.equals("0")){
+                left_door_dashswitch = false;
+            }
+            else if(Dash_Switch_Left.equals("1")){
+                left_door_dashswitch = true;
+            }
+            else{
+                System.out.println("Error with dash switch left input.");
             }
         }
+        boolean right_door_dashswitch;
+        public void Dash_SwitchR_Code(){
+            if(Dash_Switch_Right.equals("0")){
+                right_door_dashswitch = false;
+            }
+            else if(Dash_Switch_Right.equals("0")){
+                right_door_dashswitch = true;
+            }
+            else{
+                System.out.println("Error with dash switch right input.");
+            }
+
+        }
+        boolean the_masters_unlock;
+        public void Master_Lock_Code(){
+            if(Master_Unlock.equals("0")){
+                the_masters_unlock = false;
+            }
+            else if(Master_Unlock.equals("1")){
+                the_masters_unlock = true;
+            }
+            else{
+                System.out.println("Error with master lock input.");
+            }
+        }
+
+        boolean lock_of_child;
+        public void Child_Lock_Code(){
+            if(Child_Lock.equals("0")){
+                lock_of_child = false;
+            }
+            else if(Child_Lock.equals("1")){
+                lock_of_child = true;
+            }
+            else{
+                System.out.println("Error with child lock input.");
+            }
+        }
+        boolean Outside_Handle_Left_boo;
+        public void Outside_Handle_Left_Code(){
+            if(Outside_Handle_Left.equals("0")){
+                Outside_Handle_Left_boo = false;
+            }
+            else if(Outside_Handle_Left.equals("1")){
+                Outside_Handle_Left_boo = true;
+            }
+            else{
+                System.out.println("Error with Outside Handle Left Input");
+            }
+        }
+        boolean Outside_Handle_Right_boo;
+        public void Outside_Handle_Right_Code(){
+            if(Outside_Handle_Right.equals("0")){
+                Outside_Handle_Right_boo = false;
+            }
+            else if(Outside_Handle_Right.equals("1")){
+                Outside_Handle_Right_boo = true;
+            }
+            else{
+                System.out.println("Error with Outside Handle Right Input");
+            }
+        }
+        boolean Inside_Handle_Left_boo;
+        public void Inside_Handle_Left_Code(){
+        if(lock_of_child = false){
+            if(Inside_Handle_Left.equals("0")){
+                Inside_Handle_Left_boo = false;
+            }
+            else if(Inside_Handle_Left.equals("1")){
+                Inside_Handle_Left_boo = true;
+            }
+            else{
+                System.out.println("Error with Inside Handle Left Input");
+            }
+        }
+        else if(lock_of_child = true){
+            Inside_Handle_Left_boo = false;
+        }
+        }
+        boolean Inside_Handle_Right_boo;
+        public void Inside_Handle_Right_Code(){
+        if(lock_of_child = false){
+            if(Inside_Handle_Right.equals("0")){
+                Inside_Handle_Right_boo = false;
+            }
+            else if(Inside_Handle_Right.equals("1")){
+                Inside_Handle_Right_boo = true;
+            }
+            else{
+                System.out.println("Error with Inside Handle Right Input");
+            }
+        }
+        else if(lock_of_child = true){
+            Inside_Handle_Right_boo = false;
+        }
+        }
+
+
 public static void main (String[]args){
     //use charAt for if statements
     //use private then boolean or variable
