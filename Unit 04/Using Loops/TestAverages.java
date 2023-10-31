cb821b73a3f72c60043f6e2b483f978b5c583933
IL0VERAT <26mlinnboggs@derryfield.org>
2023-10-30 22:04:09 -0400

    Created TestAverages Code

---
+++ Unit 04/Using Loops/TestAverages.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class TestAverages {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        System.out.print("To end program, input a negative number.");
+    }
+}
