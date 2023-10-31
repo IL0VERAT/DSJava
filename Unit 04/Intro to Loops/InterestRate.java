ebb5bb526409080c4fd5bd33f773cc8f756fbe15
IL0VERAT <26mlinnboggs@derryfield.org>
2023-10-30 20:55:13 -0400

    Added comments to Interest Rate Code

--- Unit 04/Intro to Loops/InterestRate.java
+++ Unit 04/Intro to Loops/InterestRate.java
@@ -4,17 +4,17 @@ public class InterestRate {
     public static void main(String[]args){
     Scanner in = new Scanner(System.in);
     System.out.print("What is the interest rate (APR)? Please input as a decimal value: "); //asks user for an interest rate percentage, as a decimal value. 
-    double interest_rate = in.nextDouble();
+    double interest_rate = in.nextDouble(); // represents the interest rate
     System.out.print("What is the initial balance amount in dollars: "); //asks user for the initial balance of thier account.
-    double initial_balance = in.nextDouble();
+    double initial_balance = in.nextDouble(); //represents the initial amount
 
-    double earnings;
-    System.out.print("Year |  Int Earned ($)  |  Balance($) ");
+    double earnings; //represents the earnings of the users account with inputted interest rate
+    System.out.print("Year |  Int Earned ($)  |  Balance($) "); //lines 12 and 13: formatting 
     System.out.printf("%n--------------------------------------");
-    for (int y = 1;y < 6;y++){
-        earnings = initial_balance * interest_rate;
-        initial_balance = initial_balance + earnings;
-        System.out.printf("%n%d|%,12.02f|%,20.02f", y, earnings, initial_balance);
+    for (int y = 1;y < 6;y++){ //for loop: sets y (year) equal to 1, then adds 1 to that everytime it loops, for as long as y is less than 6. 
+        earnings = initial_balance * interest_rate; //calculates the earnings
+        initial_balance = initial_balance + earnings; //calculates the new intital balance to recheck every loop. 
+        System.out.printf("%n%d|%,12.02f|%,20.02f", y, earnings, initial_balance); //prints out the results of the loop. 
     }
     }
     }
