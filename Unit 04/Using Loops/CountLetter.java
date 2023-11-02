1390d582263a3dcad36b952de9a67924a9d7c42c
IL0VERAT <26mlinnboggs@derryfield.org>
2023-11-02 09:49:04 -0400

    Created CountLetter

---
+++ Unit 04/Using Loops/CountLetter.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class CountLetter {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("This program will accept a phrase and tell the user the amount of times the letter e appears.");
+
+        final String TARGET_LETTER = "e";
+        int counter = 0;
+        boolean breaker;
+        String phrase;
+        String sub_Phrase;
+
+        System.out.print("Enter a phrase: ");
+        
+        for (in.hasNext();;){
+            phrase = in.next();
+            breaker = false;
+            if(breaker == true){
+                break;
+            }
+        } 
+
+
+
+    }
+}
