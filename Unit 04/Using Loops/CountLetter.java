d8942101544bcb7c92d9cc8c89e569fa1305e351
IL0VERAT <26mlinnboggs@derryfield.org>
2023-11-03 07:49:38 -0400

    CountLetter Updates

--- Unit 04/Using Loops/CountLetter.java
+++ Unit 04/Using Loops/CountLetter.java
@@ -7,14 +7,18 @@ public class CountLetter {
         final String TARGET_LETTER = "e";
         boolean breaker = true;
         int count = 0;
+        
         System.out.print("Enter a phrase: ");
         String phrase = in.nextLine();
-        String seperator;
         
         for (int i = 0; i < phrase.length();i++){
-            seperator = phrase.charAt(i);
-            if(seperator.equals (TARGET_LETTER)){
+            if(String.valueOf(phrase.charAt(i)) == TARGET_LETTER){
                 count = count + 1;
+                breaker = false;
+            }
+            
+            if(breaker == true){
+                System.out.println("There is no letter e in this word or phrase");
             }
         } 
         System.out.print("The number of times the letter e appeared was " + count);
