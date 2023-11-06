cf0b426533d24c182c23fa08f5ce206a40f15806
IL0VERAT <26mlinnboggs@derryfield.org>
2023-11-06 14:15:32 -0500

    Updated DiceRoll

--- Unit 04/Using Loops/DiceRoll.java
+++ Unit 04/Using Loops/DiceRoll.java
@@ -3,11 +3,55 @@ class Dice {
     int max = 6;
     int min = 1;
     boolean breaker = false;
+    
+
     public void Roll(){
         while (breaker == false){
             random = (int)Math.floor(Math.random() * (max - min + 1) + min);
             breaker = true;
-            System.out.printf("---%n|%1d%n---",random);
+            if(random == 1){
+                System.out.printf("-----%n" + 
+                        "|   |%n" + 
+                        "| o |%n" + 
+                        "|   |%n" + 
+                        "-----");
+            }
+            if(random == 2){
+                System.out.printf("-----%n" + 
+                        "|o  |%n" + 
+                        "|   |%n" + 
+                        "|  o|%n" + 
+                        "-----");
+            }
+            if(random == 3){
+                System.out.printf("-----%n" + 
+                        "|o  |%n" + 
+                        "| o |%n" + 
+                        "|  o|%n" + 
+                        "-----");
+            }
+            if(random == 4){
+                System.out.printf("-----%n" + 
+                        "|o o|%n" + 
+                        "|   |%n" + 
+                        "|o o|%n" + 
+                        "-----");
+            }
+            if(random == 5){
+                System.out.printf("-----%n" + 
+                        "|o o|%n" + 
+                        "| o |%n" + 
+                        "|o o|%n" + 
+                        "-----");
+            }
+            if(random == 6){
+                System.out.printf("-----%n" + //
+                        "|o o|%n" + //
+                        "|o o|%n" + //
+                        "|o o|%n" + //
+                        "-----");
+            }
+            
         }
     }
         
