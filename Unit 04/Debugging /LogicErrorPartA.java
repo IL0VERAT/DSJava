827c605d690f6fde56c231a7706d537acf4aad8b
IL0VERAT <26mlinnboggs@derryfield.org>
2023-11-06 16:15:12 -0500

    App.java AKA Part C of Debugging code

--- Unit 04/Debugging /LogicErrorPartA.java
+++ Unit 04/Debugging /LogicErrorPartA.java
@@ -2,7 +2,7 @@ public class LogicErrorPartA {
     public static void main(String[] args) {
             String name = "Harry";
             System.out.println("The last letter of the name is " +
-            name.charAt(name.length() - 1)); //added a -1 add because their is 5 characters, but strings are counted from 0, the string index was out of range. 
+            name.charAt(name.length()-1)); //added a -1 add because their is 5 characters, but strings are counted from 0, the string index was out of range. 
     }
     //This was a run time error as it is was crashing when it is run. 
             
