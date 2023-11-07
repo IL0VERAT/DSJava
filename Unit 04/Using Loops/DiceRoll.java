cff6919625d5c89e8a82366b3c4a483a629c59a1
IL0VERAT <26mlinnboggs@derryfield.org>
2023-11-07 09:32:58 -0500

    Added comments to DiceRoll

--- Unit 04/Using Loops/DiceRoll.java
+++ Unit 04/Using Loops/DiceRoll.java
@@ -1,14 +1,14 @@
+//Coder: Milo Linn-Boggs
 import java.util.Scanner;
 class Dice {
-    int random;
-    int max = 6;
-    int min = 1;
-    boolean breaker = false;
+    int random; // represents the random number
+    int max = 6; //sets the highest random number
+    int min = 1; //sets the lowest random number
     
 
-    public void Roll(){
-            random = (int)Math.floor(Math.random() * (max - min + 1) + min);
-            if(random == 1){
+    public void Roll(){ //represents the roll of a die
+            random = (int)Math.floor(Math.random() * (max - min + 1) + min); //calculates the random numbe
+            if(random == 1){ //lines 11-51: represent the different faces based off what the random number is 
                 System.out.printf("-----%n" + 
                         "|   |%n" + 
                         "| o |%n" + 
@@ -57,11 +57,11 @@ class Dice {
 public class DiceRoll{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Dice dice = new Dice();
-        System.out.print("How many times would you like to roll the die: ");
+        Dice dice = new Dice(); //creates new die
+        System.out.print("How many times would you like to roll the die: "); //asks user how many time they would like to roll the die
         int input = in.nextInt();
 
-        for(int i = 0; i < input; i++){
+        for(int i = 0; i < input; i++){ //runs program number of times specified by user
             dice.Roll();
             System.out.println("");
         }
