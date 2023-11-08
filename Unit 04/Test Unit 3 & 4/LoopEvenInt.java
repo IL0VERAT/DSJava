eb165bb71c7ce8819948a39fad0aff62d330269c
IL0VERAT <26mlinnboggs@derryfield.org>
2023-11-08 11:47:41 -0500

    Added comments to the code

--- Unit 04/Test Unit 3 & 4/LoopEvenInt.java
+++ Unit 04/Test Unit 3 & 4/LoopEvenInt.java
@@ -1,9 +1,10 @@
+//Coder: Milo Linn-Boggs Date: 8 Nov. 2023
 public class LoopEvenInt{
     public static void main(String[] args) {
         int sum = 0;
-        for(int i = 2; i <= 100; i = i + 2){
-            sum = sum + i;
+        for(int i = 2; i <= 100; i = i + 2){ //runs as long as i is less than or equal to 100
+            sum = sum + i; //adds up the values
         }
-        System.out.println(sum);
+        System.out.println(sum); //prints out the final value
     }
 }
--- Unit 04/Test Unit 3 & 4/ThreeIntOrder.java
+++ Unit 04/Test Unit 3 & 4/ThreeIntOrder.java
@@ -1,16 +1,17 @@
+//Coder: Milo Linn-Boggs Date: 8 Nov. 2023
 import java.util.Scanner;
 public class ThreeIntOrder {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Enter three integers: ");
+        System.out.print("Enter three integers: "); //asks user for three integers
 
         int n1 = in.nextInt();
         int n2 = in.nextInt();
         int n3 = in.nextInt();
         int temp;
 
-        if(n1 > n2){
+        if(n1 > n2){ //lines 14 to 46: checks to see arrangement of the inputted numbers, arranged them in increasing order if needed and prints them out
             temp = n1;
             n1 = n2;
             n2 = temp;
