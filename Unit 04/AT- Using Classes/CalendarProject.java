1bb565e8dcc7ac61979f6680cd4704827c5b1602
IL0VERAT <26mlinnboggs@derryfield.org>
2023-11-13 11:01:42 -0500

    Work on CalendarProject

--- Unit 04/AT- Using Classes/CalendarProject.java
+++ Unit 04/AT- Using Classes/CalendarProject.java
@@ -1,13 +1,35 @@
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.time.DayOfWeek;
 public class CalendarProject{
     public static void main(String[] args) {
-    LocalDateTime localDate = LocalDateTime.now();
+    LocalDate rightNow = LocalDate.now();
+    int today = rightNow.getDayOfMonth();
+    //citation: stolen from Mr. Eng
+    LocalDate date = rightNow.getDayOfMonth(today - 1);
+    int firstNumber;
+    int rowNumber = 1;
+    int colNumber = 1;
+    int lastDay = rightNow.lengthOfMonth();
 
-    DateTimeFormatter dateformatter1 = DateTimeFormatter.ofPattern("MM dd yyyy");
-    System.out.println(dateformatter1.format(localDate));
+    DayOfWeek firstDay = date.getDayOfWeek();
+
+    if(firstDay == DayOfWeek.MONDAY){
+        firstNumber = 1;
+    } 
+    else if(firstDay == DayOfWeek.TUESDAY){
+        firstNumber = 2;
+    }
+    else if(firstDay == DayOfWeek.WEDNESDAY){
+        firstNumber = 3;
+    }
+    else if(firstDay == DayOfWeek.THURSDAY){
+        firstNumber = 4;
+    }
+    else if(firstDay == DayOfWeek.SATURDAY){
+        firstNumber = 5;
+    }
+
+
    
 
     }     
