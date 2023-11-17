a2f4327f1abd0e84f7e997cbaa6cd3a3ebb5fd4a
IL0VERAT <26mlinnboggs@derryfield.org>
2023-11-17 12:13:59 -0500

    Worked on CountVowel

---
+++ Unit 05/Variable Scope/CountVowel.java
@@ -0,0 +1,19 @@
+public class CountVowel {
+    public static boolean isVowel(){
+        boolean breaker;
+        for (in.hasNext()){
+            //get next word.
+            word = in.next();
+            breaker = false;
+            //do they want to quit?
+            if(breaker = true){
+                break;
+            } 
+    }
+    public static int CountVowel(String phase){
+        return phrase;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
