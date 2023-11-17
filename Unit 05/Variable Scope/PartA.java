8a8b5f8971bb8336c5d1526862ec3495e06cde32
IL0VERAT <26mlinnboggs@derryfield.org>
2023-11-17 10:15:03 -0500

    Fixed code in Part A Assignment

---
+++ Unit 05/Variable Scope/PartA.java
@@ -0,0 +1,16 @@
+//Answer to Problem: There is a duplicate local variable in this code. Input is assigned to two different methods and as different types. 
+import java.util.Scanner;
+
+public class PartA{
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+      System.out.print("What is your first name? ");
+      String name = in.next(); //change the variable to a more informative name that is unique
+      System.out.println("Hello, " + name);
+      System.out.print("How old are you? ");
+      int age = in.nextInt(); //chnage the variable here as well. 
+      age++;
+      System.out.println("Next year, you will be " + age);
+
+    }
+}
