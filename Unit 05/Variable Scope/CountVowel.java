32f4d47e7a1e92ab1fc7b1ce5a53ab847a77c6b0
IL0VERAT <26mlinnboggs@derryfield.org>
2023-11-18 18:30:20 -0500

    Worked on CountVowel

--- Unit 05/Variable Scope/CountVowel.java
+++ Unit 05/Variable Scope/CountVowel.java
@@ -1,17 +1,34 @@
 public class CountVowel {
-    public static boolean isVowel(){
-        boolean breaker;
-        for (in.hasNext()){
-            //get next word.
-            word = in.next();
-            breaker = false;
-            //do they want to quit?
-            if(breaker = true){
-                break;
+    
+    public static void isVowel(String phrase){
+        boolean tester;
+        for (int i = 0; i <= phrase.length() - 1;i++){
+            //checks to see if i is the letter e
+            if(phrase.charAt(i) == 'e'){
+                //if true, then adds one to count
+               tester = true;
             } 
+            if(phrase.charAt(i) == 'a'){
+                //if true, then adds one to count
+               tester = true;
+            } 
+            if(phrase.charAt(i) == 'i'){
+                //if true, then adds one to count
+               tester = true;
+            } 
+            if(phrase.charAt(i) == 'o'){
+                //if true, then adds one to count
+               tester = true;
+            } 
+            if(phrase.charAt(i) == 'u'){
+                //if true, then adds one to count
+               tester = true;
+            } 
+        }
+        tester = false; 
     }
     public static int CountVowel(String phase){
-        return phrase;
+        if(tester == true)
     }
     public static void main(String[] args) {
         
