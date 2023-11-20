6afe9affab58c3eb3aad7e341e457159e4e356d5
IL0VERAT <26mlinnboggs@derryfield.org>
2023-11-20 13:06:22 -0500

    Created ComparingNums

---
+++ Unit 05/AT Java Project /ComparingNums.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class ComparingNums{
+    public static int leadingNumber(String section){
+        String truncate = section.substring(0);
+        int integerTruncate = Integer.parseInt(truncate);
+        return integerTruncate;
+    }
+    public static int truncateSection(String section){
+        String lastPartTruncate = section.substring(2,5);
+        int integerPartTruncate = Integer.parseInt(lastPartTruncate);
+        return integerPartTruncate;
+
+    }
+    public static String whoIfFirst(String section1, String sectrion2){
+
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a string of numbers: ");
+        String number = in.nextLine();
+    }
+}
