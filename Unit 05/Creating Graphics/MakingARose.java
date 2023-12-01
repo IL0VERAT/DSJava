ba808619bea95e6ce11450de264d4ececcb2bb8f
IL0VERAT <26mlinnboggs@derryfield.org>
2023-12-01 10:21:57 -0500

    Created MakingARose

---
+++ Unit 05/Creating Graphics/MakingARose.java
@@ -0,0 +1,15 @@
+public class MakingARose {
+
+    public static double Mathematics{
+        int STEPS = 10;
+        for (int i = 0; i < STEPS; i++){
+        double delta = (2.0 * Math.PI)/STEPS;
+        double mForR = Math.cos(2 * delta);
+
+        
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
