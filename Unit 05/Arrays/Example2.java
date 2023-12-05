ba1af9a93bb1548fc07af88fc6e16d30cff591c1
IL0VERAT <26mlinnboggs@derryfield.org>
2023-12-05 10:43:23 -0500

    Created Example2

---
+++ Unit 05/Arrays/Example2.java
@@ -0,0 +1,7 @@
+public class Example2 {
+    public static void main(String[] args) {
+        String[]orig = {"the","quick,","brown","fox","jumped"};
+        String[]reference = orig;
+        System.out.println(reference[3]);
+    }
+}
