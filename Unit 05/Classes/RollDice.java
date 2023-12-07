c1de84e1b46802a845e53f939d05cc2661a98a40
IL0VERAT <26mlinnboggs@derryfield.org>
2023-12-06 21:43:50 -0500

    Updates to RollDice

--- Unit 05/Classes/RollDice.java
+++ Unit 05/Classes/RollDice.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
 class Die{
     //member variables
     private int value; // the value of the die that's is facing up
+    private int numTimes;
+    int max = 6; 
+    int min = 1;
+    private int randomVal;
 
-    //constructors
+    //constructor for setting initial amount
+    public Die(int initialVal){
+        //set value of die
+        this.value = initialVal;
+    }
+
     public Die(){
-        //set value of die to be 1
-        this.value = 1;
+        this.numTimes = 6; 
     }
     //getter and setter method
     public int getValue(){
@@ -18,13 +27,62 @@ class Die{
         value = newValue;
         }
     }
+    public void display(){
+        System.out.println("The inital values was "+ value);
+        value  = randomVal;
+    for(int i = 0; i < numTimes; i++){
+        randomVal = (int)Math.floor(Math.random() * (max - min + 1) + min);
+        if(randomVal == 1){  
+                System.out.printf("-----%n" + 
+                        "|   |%n" + 
+                        "| o |%n" + 
+                        "|   |%n" + 
+                        "-----");
+            }
+            if(randomVal == 2){
+                System.out.printf("-----%n" + 
+                        "|o  |%n" + 
+                        "|   |%n" + 
+                        "|  o|%n" + 
+                        "-----");
+            }
+            if(randomVal == 3){
+                System.out.printf("-----%n" + 
+                        "|o  |%n" + 
+                        "| o |%n" + 
+                        "|  o|%n" + 
+                        "-----");
+            }
+            if(randomVal == 4){
+                System.out.printf("-----%n" + 
+                        "|o o|%n" + 
+                        "|   |%n" + 
+                        "|o o|%n" + 
+                        "-----");
+            }
+            if(randomVal == 5){
+                System.out.printf("-----%n" + 
+                        "|o o|%n" + 
+                        "| o |%n" + 
+                        "|o o|%n" + 
+                        "-----");
+            }
+            if(randomVal == 6){
+                System.out.printf("-----%n" + //
+                        "|o o|%n" + //
+                        "|o o|%n" + //
+                        "|o o|%n" + //
+                        "-----");
+            }
+        }
+    }
 }
 public class RollDice{
     public static void main(String[] args) {
-        Die die = new Die();
-        System.out.println(die.getValue());
-        //change value 
-        die.setValue(5);
-        System.out.print(die.getValue());
+        Scanner in = new Scanner(System.in);
+        System.out.print("What should the inital value of the die be: ");
+        int input = in.nextInt();
+        Die die = new Die(input);
+        die.display();
     }
 }
