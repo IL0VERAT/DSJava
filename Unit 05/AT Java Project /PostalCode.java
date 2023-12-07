37f3f1fc9742ad7a1864d82d40d6b5d80fb67125
IL0VERAT <26mlinnboggs@derryfield.org>
2023-12-07 11:24:45 -0500

    Create PostalCode

---
+++ Unit 05/AT Java Project /PostalCode.java
@@ -0,0 +1,21 @@
+public class PostalCode{
+    public static String getEncodedDigit(int d){
+        String barTransformer = switch(d) {
+            case 1 -> ":::||";
+            case 2 -> "::|:|";
+            case 3 -> "::||:";
+            case 4 -> ":|::|";
+            case 5 -> ":|:|:";
+            case 6 -> ":||::";
+            case 7 -> "|:::|";
+            case 8 -> "|::|:";
+            case 9 -> "|:|::";
+            case 0 -> "||:::";
+            default -> "XXXXX";
+        };
+        return barTransformer;
+    }
+    public static void main(String[]args){
+        System.out.print("Hello");
+    }
+}
