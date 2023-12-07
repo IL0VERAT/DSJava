8acb7c32da73fc19701d005fd0e5ff3f6877c864
IL0VERAT <26mlinnboggs@derryfield.org>
2023-12-07 10:43:26 -0500

    Added comments to RollDice

--- Unit 05/Classes/RollDice.java
+++ Unit 05/Classes/RollDice.java
@@ -1,11 +1,12 @@
+//Coder: Milo Linn-Boggs Date: 7 Dec. 2023
 import java.util.Scanner;
 class Die{
     //member variables
     private int value; // the value of the die that's is facing up
-    private int min = 1; 
-    private int max = 6;
+    private int min = 1; //minimum value for the die value
+    private int max = 6; //maximum value for the die value
 
-    //constructor for setting initial amount
+    //constructor for setting initial amount (overloaded)
     public Die(int initialVal){
         //set value of die
         this.value = initialVal;
@@ -13,24 +14,30 @@ class Die{
         setValue(initialVal);
     }
 
+    //constructor for setting initial amount (no arguments)
     public Die(){
         this.value = 1; 
     }
-    //getter and setter method
-    public int getValue(){
-        return value;
-    }
 
+    //setter method: ensures the users inputted data is within the parameters
     public void setValue(int newValue){
         // the only valid values range from 1 to 6
         if(newValue > 0 && newValue < 7){
+            System.out.println("Heres the die with arguments");
         value = newValue;
+        // if invalid value, prints error message
+        } else {
+            System.out.println("ERROR: INPUT NUMBER BETWEEN 1 AND 6");
         }
     }
+
+    //roll method: randomizes the die
     public void Roll(){
         value = (int)Math.floor(Math.random() * (max - min + 1) + min);
 
     }
+
+    //displays the die
     public void display(){
         if(value == 1){  
                 System.out.printf("-----%n" + 
@@ -77,24 +84,26 @@ class Die{
             }
         }
     }
-
-
 public class RollDice{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        //asks the user for the inital value of the die
         System.out.print("What should the inital value of the die be: ");
         int input = in.nextInt();
+        
+        //displays the inital die face
         Die die = new Die(input);
-        System.out.printf("%nHeres the die with arguments%n");
         die.display();
+        
+        //randomizes the die and rolls it 3 times
         for(int i = 0; i < 3; i++){
-        die.Roll();
-        System.out.printf("%nHeres the rolled die.");
-        System.out.println("");
-        die.display();
-        System.out.println("");
+            die.Roll();
+            System.out.printf("%nHeres the rolled die.");
+            System.out.println("");
+            die.display();
         }
     
+        //creates a new die with no arguments and prints it
         Die die2 = new Die();
         System.out.println("");
         System.out.println("Here's the die with no arguments.");
