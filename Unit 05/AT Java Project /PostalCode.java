fa58c2e0450cdf2584b4ac2b1b1c0134d441122c
IL0VERAT <26mlinnboggs@derryfield.org>
2023-12-10 09:56:54 -0500

    Update PostalCode

--- Unit 05/AT Java Project /PostalCode.java
+++ Unit 05/AT Java Project /PostalCode.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class PostalCode{
     public static String getEncodedDigit(int d){
         String barTransformer = switch(d) {
@@ -17,13 +18,32 @@ public class PostalCode{
     }
 
     public static int getCheckDigit(int zipcode){
-        int sum,digit,number = 0;
+        int sum = 0;
+        int digit = 0;
         int workingNum = zipcode;
         while(workingNum > 0){
-            
+            digit = workingNum%10;
+            sum = sum + digit;
+            workingNum = workingNum/10;
         }
+        int a = (sum / 10) * 10; 
+        int b = a + 10;
+        int answer = (sum - a > b - sum)? b : a;
+        
+        int checkDigit = Math.abs(answer - sum);
+
+        System.out.print(checkDigit);
+        return answer;
+       
     }
     public static void main(String[]args){
+        PostalCode postalcode = new PostalCode();
+        Scanner in = new Scanner (System.in);
+        System.out.print("Enter a zipcode: ");
+        int zip = in.nextInt();
+        PostalCode.getCheckDigit(zip);
+
+
 
     }
 }
