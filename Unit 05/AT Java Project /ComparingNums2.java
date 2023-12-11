8c0e46e815002eb7f78a153bee159acb5d9120f8
IL0VERAT <26mlinnboggs@derryfield.org>
2023-12-11 15:07:52 -0500

    Update ComparingNums2

--- Unit 05/AT Java Project /ComparingNums2.java
+++ Unit 05/AT Java Project /ComparingNums2.java
@@ -1,35 +1,50 @@
 import java.util.Scanner;
 public class ComparingNums2 {
-    public static int I;
+    public static int i;
     public static String truncator;
     public static int leadingNumber(String section){
-        I = section.indexOf(".");
-        String nString = section.substring(0, I);
-        int n = Integer.valueOf(nString);
+        int n = 0;
+        i = section.indexOf(".");
+        if (i >= 0){
+        String nString = section.substring(0, i);
+        n = Integer.valueOf(nString);
+        } else {
+        n = Integer.valueOf(section);
+        }
         return n;
     }
     public static String truncateSection(String section){
-        truncator = section.substring(I+1);
+        if(i >= 0){
+        truncator = section.substring(i+1);
+        } else {
+            truncator = "";
+        }
         return truncator;
     }
     public static void whoIsFirst(String section1, String section2){
-        int leadWork1 = ComparingNums2.leadingNumber(section1);
-        int leadWork2 = ComparingNums2.leadingNumber(section2);
-        String truncateWork1 = ComparingNums2.truncateSection(section1);
-        String truncateWork2 = ComparingNums2.truncateSection(section2);
+        String truncateWork1 = section1;
+        String truncateWork2 = section2;
         boolean breaker = true;
 
     while(breaker == true){
+        if(truncateWork1.length() == 0){
+            System.out.print(section2 + "is bigger");
+        } else if(truncateWork2.length() == 0){
+            System.out.print(section1 + "is bigger");
+        }
+    
+        int leadWork1 = ComparingNums2.leadingNumber(truncateWork1);
+        int leadWork2 = ComparingNums2.leadingNumber(truncateWork2);
+         truncateWork1 = ComparingNums2.truncateSection(truncateWork1);
+         truncateWork2 = ComparingNums2.truncateSection(truncateWork2);
+
         if(leadWork1 > leadWork2){
             System.out.print(section1 +" is the bigger number.");
             breaker = false;
         } else if(leadWork2 > leadWork1){
             System.out.print(section2 +" is the bigger number.");
             breaker = false;
-        } else {
-            leadWork1 = ComparingNums2.leadingNumber(truncateWork1);
-            leadWork2 = ComparingNums2.leadingNumber(truncateWork2);
-        }
+        } 
     }
 
     
