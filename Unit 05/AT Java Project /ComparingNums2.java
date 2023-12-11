16b75aa61a48e0e25efd229795718097187d2b04
IL0VERAT <26mlinnboggs@derryfield.org>
2023-12-11 12:11:57 -0500

    Update ComparingNums2

--- Unit 05/AT Java Project /ComparingNums2.java
+++ Unit 05/AT Java Project /ComparingNums2.java
@@ -1,25 +1,28 @@
 import java.util.Scanner;
 public class ComparingNums2 {
     public static int I;
+    public static String truncator;
     public static int leadingNumber(String section){
         I = section.indexOf(".");
         String nString = section.substring(0, I);
         int n = Integer.valueOf(nString);
         return n;
     }
-
     public static String truncateSection(String section){
-        String truncator = section.substring(I);
-        
+        truncator = section.substring(I+1);
+        return truncator;
     }
     public static void whoIsFirst(String section1, String section2){
-
+        ComparingNums2.leadingNumber(section1);
+        ComparingNums2.truncateSection(section1);
+        ComparingNums2.leadingNumber(section2);
+        ComparingNums2.truncateSection(section2);
     }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        
         System.out.print("Enter a number: ");
         String section1 = in.nextLine();
-
         System.out.print("Enter another  number: ");
         String section2 = in.nextLine();
     }
