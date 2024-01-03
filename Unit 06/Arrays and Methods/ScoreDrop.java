5ee85cf754a60dde479b9fc8681428eaf43653b5
IL0VERAT <26mlinnboggs@derryfield.org>
2024-01-03 11:33:40 -0500

    Update ScoreDrop

--- Unit 06/Arrays and Methods/ScoreDrop.java
+++ Unit 06/Arrays and Methods/ScoreDrop.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
+
 public class ScoreDrop {
-    static double least = 0;
+    static double least = 1000;
     static int currentSize = 0;
 
-    public static void dropper(double[] testArrays){
+    public static void dropper(double[] testArrays) {
         double searchedValue = least;
         int pos = 0;
         boolean found = false;
-        
+
+        // KEEP ORDER - DO BEFORE SUBMISSION!!!
         while (pos < testArrays.length && !found) {
             if (testArrays[pos] == searchedValue) {
                 testArrays[pos] = testArrays[currentSize - 1];
@@ -20,42 +22,43 @@ public class ScoreDrop {
 
         for (int i = 0; i < currentSize; i++) {
             System.out.println(testArrays[i]);
-            }
+        }
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         double[] testValues = new double[25];
-    while(true){
-        for(int i = 0; i < testValues.length; i++){
+        while (true) {
+            for (int i = 0; i < testValues.length; i++) {
 
-            //asks the user for test scores
-            System.out.print("Enter a test score (enter q to quit or y to drop the lowest test score): ");
-            
-            //checks to see what the smallest test score is
-            if(testValues[i] < least){
-                least = testValues[i];
-            } 
-            
-            //stops program if user inputs q or Q
-            if(in.hasNext("q")||in.hasNext("Q")){
-                for (int j = 0; j < currentSize; j++) {
-            System.out.println(testValues[j]);
+                // asks the user for test scores
+                System.out.print("Enter a test score (enter q to quit or y to drop the lowest test score): ");
+
+                // stops program if user inputs q or Q
+                if (in.hasNext("q") || in.hasNext("Q")) {
+                    in.next();
+                    for (int j = 0; j < currentSize; j++) {
+                        System.out.println(testValues[j]);
+                    }
+                    break;
+                }
+
+                // assigns the user input to the corresponding place in the array
+                testValues[i] = in.nextDouble();
+                currentSize++;
+
+                // checks to see what the smallest test score is
+                //SET FLAG 
+                if (testValues[i] < least) {
+                    least = testValues[i];
+                }
             }
             break;
-            }
-
-            //eliminates the lowest score if user inputs y or Y
-            if(in.hasNext("y")||in.hasNext("Y")){
-                in.next();
-                ScoreDrop.dropper(testValues);
-            }
-
-            //assigns the user input to the corresponding place in the array
-            testValues[i] = in.nextDouble();
-            currentSize++;
-            }
-            break;
+        }
+        System.out.println("Would you like to drop your lowester score? Enter Y for yes or N for no: ");
+        String yesNo = in.next();
+        if (yesNo.equals("y") || yesNo.equals("Y")) {
+            ScoreDrop.dropper(testValues);
         }
 
     }
