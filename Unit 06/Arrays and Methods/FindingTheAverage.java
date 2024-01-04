998b0ee989bfae3878e47a6d5e45f7a28920a607
IL0VERAT <26mlinnboggs@derryfield.org>
2024-01-04 17:51:31 -0500

    Update FindingTheAverage

--- Unit 06/Arrays and Methods/FindingTheAverage.java
+++ Unit 06/Arrays and Methods/FindingTheAverage.java
@@ -5,26 +5,24 @@ public class FindingTheAverage {
         for(int i = 0; i < currentSize; i++){
             sum += valueArray[i];
         }
-        System.out.println(sum);
+        System.out.println(sum/currentSize);
     }
     public static void main(String[] args) {
         Scanner in = new Scanner (System.in);
         final int numValues = 100;
         int currentSize = 0;
         double [] values = new double [numValues];
-        while(true){
-            for(int i = 0; i < numValues; i++){
-                System.out.print("Enter a random double values or q to quit.");
+        for(int i = 0; i < numValues; i++){
+            System.out.print("Enter a random double values or q to quit.");
 
             if(in.hasNext("q")||in.hasNext("Q")){
-                    break;
-            } else {
+                in.next();
+                FindingTheAverage.findAverage(currentSize, values);
+            } else { 
                 values[i] = in.nextDouble();
                 currentSize++;
-            } 
-            break; 
+            }
+
         }
     }
-
-}
 }
