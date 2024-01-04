681bd5cbdf0ed755cf090b24c701088e5e7cf8fb
IL0VERAT <26mlinnboggs@derryfield.org>
2024-01-04 18:28:51 -0500

    Added comments

--- Unit 06/Arrays and Methods/FindingTheAverage.java
+++ Unit 06/Arrays and Methods/FindingTheAverage.java
@@ -1,13 +1,19 @@
+//Coder: Milo Linn-Boggs Date: 01/04/24
 import java.util.Arrays;
 import java.util.Scanner;
 public class FindingTheAverage {
+
+    //method to find the average of the inputted numbers
     public static void findAverage (int currentSize, double[] valueArray){
         double[] copyValue = Arrays.copyOf(valueArray, valueArray.length);
         double sum = 0;
+        //code to find sum of values
         for(int i = 0; i < currentSize; i++){
             sum += copyValue[i];
         }
+        //code to find average of values
         double total = sum/currentSize;
+        //prints out average
         System.out.printf("%nThe average of the inputted numbers is %.2f.", total);
     }
     public static void main(String[] args) {
@@ -15,9 +21,11 @@ public class FindingTheAverage {
         final int numValues = 100;
         int currentSize = 0;
         double [] values = new double [numValues];
+        //asks user to input numbers
         for(int i = 0; i < numValues; i++){
             System.out.print("Enter a random double values or q to quit: ");
 
+            //checks to see if user stopped program
             if(in.hasNext("q")||in.hasNext("Q")){
                 in.next();
                 FindingTheAverage.findAverage(currentSize, values);
