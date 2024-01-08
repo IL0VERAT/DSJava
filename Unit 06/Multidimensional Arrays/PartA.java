8702d2fb2a0c1e9023690392f1462a371998b3d0
IL0VERAT <26mlinnboggs@derryfield.org>
2024-01-08 17:42:07 -0500

    Update PartA

--- Unit 06/Multidimensional Arrays/PartA.java
+++ Unit 06/Multidimensional Arrays/PartA.java
@@ -1,5 +1,22 @@
 public class PartA{
     public static void main(String[] args) {
-        
+   
+    int[][]arrayOne = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
+
+    int[][]arrayTwo = {
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0}
+    };
+
+    int[][]arrayThree = {
+        {8, 6, 7},
+        {5, 3, 0}
+    };
+
     }
 }
