5249a045fc28433f653ff782ba882f1d69ac1d4a
IL0VERAT <26mlinnboggs@derryfield.org>
2024-01-08 17:21:27 -0500

    Work on PartB

--- Unit 06/Multidimensional Arrays/PartB.java
+++ Unit 06/Multidimensional Arrays/PartB.java
@@ -1,5 +1,32 @@
 public class PartB {
+    public static void print(int[][] chart){
+        for (int row = 0; row < chart.length; row++) {
+            for (int col = 0; col < chart[0].length; col++) {
+                System.out.print(chart[row][col]);
+                // only print a comma if it's not the last column.
+                if (col < chart[0].length - 1) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
     public static void main(String[] args) {
-        
+        int arrayOne[][] = {
+            {8, 6, 7},
+            {5, 3, 0}
+        };
+        System.out.println("First Array: ");
+        PartB.print(arrayOne);
+
+        int arrayTwo[][] = {
+            {0, 7, 6, 5},
+            {2, 2, 0, 1},
+            {8, 4, 5, 7}
+        };
+        System.out.println("");
+        System.out.println("Second Array: ");
+        PartB.print(arrayTwo);
     }
 }
