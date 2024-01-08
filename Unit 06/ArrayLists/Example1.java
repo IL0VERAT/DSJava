061c6abaf82dd3445e1132dfd8da87be91a7163c
IL0VERAT <26mlinnboggs@derryfield.org>
2024-01-08 17:05:05 -0500

    Update Multidimensional Arrays Part A and B

---
+++ Unit 06/ArrayLists/Example1.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+public class Example1{
+    public static void main(String[] args) {
+        ArrayList<Integer> values = new ArrayList<Integer>();
+        values.add(1);
+        values.add(42);
+    }
+}
