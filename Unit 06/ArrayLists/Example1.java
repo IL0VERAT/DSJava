5a788fd4d29edc0c37e9745029a5efa71875a6b4
IL0VERAT <26mlinnboggs@derryfield.org>
2024-01-11 10:27:33 -0500

    Updated TempTracker

--- Unit 06/ArrayLists/Example1.java
+++ Unit 06/ArrayLists/Example1.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
 public class Example1{
     public static void main(String[] args) {
-        ArrayList<Integer> values = new ArrayList<Integer>();
-        values.add(1);
-        values.add(42);
+        3
     }
 }
--- Unit 06/ArrayLists/TempTracker.java
+++ Unit 06/ArrayLists/TempTracker.java
@@ -1,19 +1,45 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+
 public class TempTracker {
+    public static void printer(ArrayList<Integer> temps, Integer least) {
+        System.out.printf("Temperature Values: %n");
+
+        for (int i = 0; i < temps.size(); i++) {
+            System.out.print(temps.get(i));
+            if (temps.get(i) == least) {
+                System.out.print(" <== ");
+                System.out.println("");
+            } else {
+                System.out.println("");
+            }
+
+        }
+        System.out.println("");
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        ArrayList<Double> temps = new ArrayList<Double>();
+        ArrayList<Integer> temps = new ArrayList<Integer>();
+        Integer least = 0;
         while (true) {
             System.out.println("Enter a temperature: ");
 
-            if (in.hasNext("q")||in.hasNext("Q")) {
+            if (in.hasNext("q") || in.hasNext("Q")) {
                 in.next();
                 break;
             } else {
-                temps.add(in.nextDouble());
+                Integer val = in.nextInt();
+                if(temps.size() < 1){
+                    least = val;
+                } else if (val < least) {
+                    least = val;
+                }
+                temps.add(val);
             }
+
         }
+        TempTracker.printer(temps, least);
 
     }
 }
