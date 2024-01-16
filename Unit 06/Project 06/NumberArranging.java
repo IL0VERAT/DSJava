c71e6fd70d00634d15de3d7d1257ff80c86931ff
IL0VERAT <26mlinnboggs@derryfield.org>
2024-01-16 18:22:05 -0500

    Created NumberArranging
    This code manipulates 10 random integers.
    Firstly, it prints the integers at even indices.
    Secondly, the code prints the even integers
    Thirdly, the piece print the integers in reverse order
    Lastly, the code prints the first and last integer in the sequence

--- Unit 06/Project 06/NumberArranging.java
+++ Unit 06/Project 06/NumberArranging.java
@@ -1,9 +1,37 @@
-public class NumberArranging{
+public class NumberArranging {
     public static void main(String[] args) {
-    int[] random = new int[10];
-    for(int i = 1; i <= 10; i++){
-        double r = Math.random();
-        System.out.print(r);
-    }
+        final int LENGTH = 10;
+        int[] random = new int[LENGTH];
+        System.out.println("Original: ");
+        for (int i = 0; i < LENGTH; i++) {
+            random[i] = (int)(Math.random() * 100);
+            System.out.print(random[i] + " ");
+        }
+        System.out.println(" ");
+
+        System.out.println("Numbers At Even Indices: ");
+        for(int i = 0; i < LENGTH; i += 2){
+            System.out.print(random[i] + " ");
+        }
+        System.out.println(" ");
+
+        System.out.println("Even Numbers Only: ");
+        for(int i = 0; i < LENGTH; i++){
+            if(random[i]%2 == 0){
+                System.out.print(random[i] + " ");
+            }
+        }
+        System.out.println(" ");
+
+        System.out.println("Numbers In Reverse Order: ");
+        for(int i = LENGTH - 1; i > -1; i--){
+            System.out.print(random[i] + " ");
+        }
+        System.out.println(" ");
+
+        System.out.println("First and Last Numbers: ");
+        System.out.print(random[(LENGTH-1)-(LENGTH-1)] + " ");
+        System.out.print(random[LENGTH-1] + " ");
+
     }
 }
