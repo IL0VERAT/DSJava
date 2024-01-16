f6247becd0b706bd564740bbde5b81b88c43ee5a
IL0VERAT <26mlinnboggs@derryfield.org>
2024-01-16 17:54:42 -0500

    Added comments to AlternatingSum

--- Unit 06/Project 06/AlternatingSum.java
+++ Unit 06/Project 06/AlternatingSum.java
@@ -1,12 +1,17 @@
+//Coder: Milo Linn-Boggs Date: 16 Jan. 2023
 import java.util.Scanner;
 import java.util.ArrayList;
 public class AlternatingSum {
+
+    //method to add or subtract the numbers in the ArrayList
     public static void calculator (ArrayList<Integer> workingNum){
         int total = 0; 
         for(int i = 0; i < workingNum.size();i++){
+            //adds the even index numbers
             if(i%2 == 0){
                 total = total += workingNum.get(i);
             } 
+            //adds the odd index numbers
             if (i%2 == 1){
                 total = total -= workingNum.get(i);
             }
@@ -16,7 +21,7 @@ public class AlternatingSum {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         ArrayList<Integer> integers = new ArrayList<Integer>();
-        //asks user to input tests scores 
+        //asks user to input integer values
         while (true) {
             System.out.println("Enter a integer value or q to quit: ");
 
