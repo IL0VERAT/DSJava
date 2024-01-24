5d62c958d9c8e9efcfdc9c83a93fe562b487c12e
IL0VERAT <26mlinnboggs@derryfield.org>
2024-01-23 20:47:20 -0500

    Update DateAndTime

--- Unit 07/AT Using Packages/DateAndTime.java
+++ Unit 07/AT Using Packages/DateAndTime.java
@@ -1,5 +1,12 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
 public class DateAndTime{
     public static void main(String[] args) {
         
+        LocalDate today = LocalDate.now();
+        System.out.println("Todays Date: " + today);
+
+        LocalTime time = LocalTime.now();
+        System.out.println("Current Time: " + time);
     }
 }
