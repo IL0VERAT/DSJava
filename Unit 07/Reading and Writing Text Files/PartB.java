4839b7b2450b735c93a4f255d72b6bf558575aa1
IL0VERAT <26mlinnboggs@derryfield.org>
2024-01-23 20:02:46 -0500

    Update PartB

--- Unit 07/Reading and Writing Text Files/PartB.java
+++ Unit 07/Reading and Writing Text Files/PartB.java
@@ -7,15 +7,21 @@ public class PartB {
     public static void main(String[] args) throws FileNotFoundException {
         File inputFile = new File("quizzes.txt");
         Scanner in = new Scanner(inputFile);
-
         File outputFile = new File("quizzesMod.txt");
         PrintWriter out = new PrintWriter(outputFile);
+        double counter = 0;
+        double total = 0;
+        double scores = 0;
 
         while (in.hasNext()) {
-            double scores = in.nextDouble();
-            out.printf("%6.2f%n", scores);
+            counter++;
+            scores = in.nextDouble();
+            out.printf("Quiz %03f:%7.2f%n", counter, scores);
+            total += scores;
         }
+        //prints out the average of the test scores
         out.println("----------------");
+        out.printf("AVERAGE: %7.2f%n", (total / counter));
         in.close();
         out.close();
     }
