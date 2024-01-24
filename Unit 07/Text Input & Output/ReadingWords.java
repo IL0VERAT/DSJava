e5bdbc45407baf8ead205c784a866504daf1b535
IL0VERAT <26mlinnboggs@derryfield.org>
2024-01-24 10:46:26 -0500

    Created ReadingWords
    Example from lesson 20 in AT Java

---
+++ Unit 07/Text Input & Output/ReadingWords.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class ReadingWords {
+   public static void main(String[] args) throws FileNotFoundException {
+       File inputFile = new File("poem.txt");
+       Scanner in = new Scanner(inputFile);
+       int count = 0;
+       while (in.hasNext()) {
+           String word = in.next();
+           System.out.println(word);
+           count++;
+       }
+       System.out.printf("%d words were scanned in.%n", count);
+       in.close();
+   }
+}
