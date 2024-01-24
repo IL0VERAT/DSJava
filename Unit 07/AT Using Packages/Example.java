e103ccef8e819988c6e2868592d9c6d9d12ea54f
IL0VERAT <26mlinnboggs@derryfield.org>
2024-01-24 12:14:03 -0500

    Added comments

--- Unit 07/AT Using Packages/Example.java
+++ Unit 07/AT Using Packages/Example.java
@@ -1,14 +1,19 @@
+//Coder: Milo Linn-Boggs Date: 
+//imports the custom package
 import org.derryfield.math.Geometry;
 import java.util.Scanner;
 public class Example {
     public static void main(String[] args) {
         Scanner in = new Scanner (System.in);
+        //asks the user for the radius of a circle
         System.out.println("Enter the radius of a circle: ");
         double radius = in.nextDouble();
 
+        //uses the custom functions of the geometry class
        double surf =  Geometry.calcSurfSphere(radius);
        double vol =  Geometry.calcVolSphere(radius);
 
+       //prints out the answers
         System.out.printf("%nThe volume of the sphere with a radius of %.2f is %.2f", radius, vol);
         System.out.printf("%nThe surface area of the sphere with a radius of %.2f is %.2f", radius, surf);
     }
