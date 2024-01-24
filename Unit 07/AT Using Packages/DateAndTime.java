9c5bfad6e631df524dd3d5d38aad8a3d56c6110c
IL0VERAT <26mlinnboggs@derryfield.org>
2024-01-23 20:48:20 -0500

    Added comments

--- Unit 07/AT Using Packages/DateAndTime.java
+++ Unit 07/AT Using Packages/DateAndTime.java
@@ -1,11 +1,16 @@
+//Coder: Milo Linn-Boggs Date: 23 Jan. 2023
+
+//imports the needed packages
 import java.time.LocalDate;
 import java.time.LocalTime;
 public class DateAndTime{
     public static void main(String[] args) {
         
+        //prints the current date
         LocalDate today = LocalDate.now();
         System.out.println("Todays Date: " + today);
 
+        //prints the current time
         LocalTime time = LocalTime.now();
         System.out.println("Current Time: " + time);
     }
