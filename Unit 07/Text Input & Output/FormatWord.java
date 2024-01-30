e792fb2af2e30ab8e88d2a9de1df504c768ef828
IL0VERAT <26mlinnboggs@derryfield.org>
2024-01-30 09:51:51 -0500

    Created FormatWord

--- Unit 07/Text Input & Output/FormatWord.java
+++ Unit 07/Text Input & Output/FormatWord.java
@@ -3,17 +3,19 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class FormatWord {
-   public static void main(String[] args) throws FileNotFoundException {
-       File inputFile = new File("customerdata.txt");
-       Scanner in = new Scanner(inputFile);
-       in.useDelimiter("[,\\s\\t\\n]+");
-       int count = 0;
-       while (in.hasNext()) {
-           String word = in.next();
-           System.out.println(word);
-           count++;
-       }
-       System.out.printf("%d words were scanned in.%n", count);
-       in.close();
-   }
+public static void main(String[] args) throws FileNotFoundException {
+    File inputFile = new File("customerdata.txt");
+    Scanner in = new Scanner(inputFile);
+    in.useDelimiter("[,\\n\\t\\s]+");
+    System.out.println("---------------------------------------");
+    System.out.println(" ID  |    Name     |     Balance ");
+    System.out.println("---------------------------------------");
+    int count = 0;
+    while (in.hasNext()) {
+        String word = in.next();
+        System.out.print(word);
+        count++;
+    }
 }
+}
+
