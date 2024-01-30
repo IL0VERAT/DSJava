828b6b94f60629a77c4f2c1700e3b6b2ba5e01bd
IL0VERAT <26mlinnboggs@derryfield.org>
2024-01-30 11:51:05 -0500

    Added comments to FormatWord

--- Unit 07/Text Input & Output/FormatWord.java
+++ Unit 07/Text Input & Output/FormatWord.java
@@ -1,15 +1,20 @@
+//Coder: Milo Linn-Boggs Date: 30 Jan. 2024
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class FormatWord {
 public static void main(String[] args) throws FileNotFoundException {
+    //scans in the file
     File inputFile = new File("customerdata.txt");
     Scanner in = new Scanner(inputFile);
+    //sets the delimiters
     in.useDelimiter("[,\\n\\t\\s]+");
+    //prints brackets
     System.out.println("---------------------------------------");
     System.out.println("   ID  |    Name     |     Balance ");
     System.out.println("---------------------------------------");
+    //prints out the characters from the file
     while (in.hasNext()) {
         // read in id
         int id = in.nextInt();
