13ff5111fedef0c48277014aaad8ac02bfa8d8dd
IL0VERAT <26mlinnboggs@derryfield.org>
2024-02-01 10:54:59 -0500

    Updated Unemployment

--- Unit 07/Processing Files/Unemployment.java
+++ Unit 07/Processing Files/Unemployment.java
@@ -1,29 +1,38 @@
+
+//Coder: Milo Linn-Boggs Date: 1 Feb. 2024
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-//ASSUME NUMBER NEXT TO NAME -  has to be scanning in the type it expects
+
 public class Unemployment {
     public static void main(String[] args) throws FileNotFoundException {
-    File inputFile = new File("Unemployment.txt"); //can use full name here
-    Scanner in = new Scanner(inputFile);
-    //process file line by line
-    //loop where there is still a next line in the file. 
-    while (in.hasNextLine()) {
-        String line = in.nextLine();
+        File inputFile = new File("Unemployment.txt"); // imports the .txt file
+        Scanner in = new Scanner(inputFile);
 
-        Scanner lineScanner = new Scanner(line);
-        lineScanner.useDelimiter("[\\n]+");
+        // creates the top title
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("                   Top 5 Unemployment Rates                ");
+        System.out.println("--------------------------------------------------------------");
+        // process file line by line
+        // loop where there is still a next line in the file.
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
 
-        String name  = lineScanner.next().trim();
-        String nextWord = lineScanner.next().trim(); //need to trim, as could mess up code later
-        nextWord = nextWord.replaceAll(",", "");
-        double gdp = Double.valueOf(nextWord);
-        nextWord = lineScanner.next().trim();
-        
-        int rank  = Integer.valueOf(nextWord);
-        String region = lineScanner.next().trim();
+            Scanner lineScanner = new Scanner(line);
+            lineScanner.useDelimiter("[,\\n]+");
 
-        System.out.printf("Ranked #%3d: %-16s | $%,-24.2f | %-20s%n",rank, name, gdp, region);
+            String name = lineScanner.next().trim();
+            String nextWord = lineScanner.next().trim();
+            nextWord = nextWord.replaceAll(",", "");
+            double unempolyment = Double.valueOf(nextWord);
+            nextWord = lineScanner.next().trim();
+
+            int rank = Integer.valueOf(nextWord);
+            String region = lineScanner.next().trim();
+
+            // prints formatted output
+            System.out.printf("Ranked #%3d: %-16s | %,-5.2f%% | %-20s%n", rank, name, unempolyment, region);
+        }
+        System.out.println("--------------------------------------------------------------");
     }
-}
 }
