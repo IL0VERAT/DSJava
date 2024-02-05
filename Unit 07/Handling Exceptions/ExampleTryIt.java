e42b131afbe735355852933b49a1c2de332a64ed
IL0VERAT <26mlinnboggs@derryfield.org>
2024-02-05 10:13:22 -0500

    Upload

---
+++ Unit 07/Handling Exceptions/ExampleTryIt.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class ExampleTryIt{
+public static void main(String[] args) throws FileNotFoundException {
+    Scanner inKeyboard = new Scanner(System.in);
+    boolean fileOpened = false;
+    String name = "ryIt.txt";
+    while (fileOpened == false) {
+        try {
+            File inputFile = new File(name);
+            Scanner in = new Scanner(inputFile);
+            while (in.hasNext()) {
+                String word = in.next();
+                System.out.println(word);
+            }
+            in.close();
+            fileOpened = true;
+        } catch (FileNotFoundException exception) {
+            System.err.println("Error: unable to find your stupid file.");
+            System.err.println(exception.getMessage());
+            return;
+        }
+    }
+}
+
+    
+}
