f32d5b679d7f899aabb88be73498afdd268183bc
IL0VERAT <26mlinnboggs@derryfield.org>
2024-02-11 19:27:42 -0500

    Added comments

--- Unit 07/Handling Exceptions/PartA.java
+++ Unit 07/Handling Exceptions/PartA.java
@@ -1,22 +1,27 @@
+//Coder: Milo Linn-Boggs Date: Feb 9th (I believe) 2024
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class PartA{
 public static void main(String[] args) throws FileNotFoundException {
     Scanner inKeyboard = new Scanner(System.in);
+    //asks user to input file name or # to quit
     System.out.println("Enter a file name or # to quit: ");
     boolean fileOpened = false;
     String name = inKeyboard.next();
+    //if the input is not a #, runs
     while (fileOpened == false && !name.trim().equals("#")) {
         try {
             File inputFile = new File(name);
             Scanner in = new Scanner(inputFile);
+            //prints out the contents of the file
             while (in.hasNext()) {
                 String word = in.next();
                 System.out.println(word);
             }
             in.close();
             fileOpened = true;
+            //catchs a FileNotFound Exception
         } catch (FileNotFoundException exception) {
             System.out.println("Error: unable to find the file named \"" + name + "\".");
             System.out.print("Enter the correct name of the file to open: ");
--- Unit 07/Handling Exceptions/PartB.java
+++ Unit 07/Handling Exceptions/PartB.java
@@ -1,22 +1,26 @@
+//Coder: Milo Linn-Boggs Date: 11 Feb. 2024 (SORRY THIS IS A WEE BIT LATE)
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
 public class PartB {
     public static void main(String[] args) {
         Scanner in;
+        //checks for FileNotFoundException
         try {
             File input = new File("birthyears.txt");
             in = new Scanner(input).useDelimiter("[,\\s]+");
-        } catch (Exception FileNotFoundException){
+        } catch (FileNotFoundException Exception){
             System.out.println("File not found");
             in = new Scanner(System.in);
         }
 
+        //Prints out data and checks for NumberFormatException
         while(in.hasNext()){
             String name = in.next();
             try{
                 int age = Integer.parseInt(in.next());
                 System.out.println(name + "'s birthday is " + age);
-            } catch (Exception NumberFormatException){
+            } catch (NumberFormatException Exception){
                 System.out.println(name + "'s birthday is unknown");
             }
         }
