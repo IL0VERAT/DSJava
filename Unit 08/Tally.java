9fc1828cfd60657c6232ce219d12fa414ea0e2a6
IL0VERAT <26mlinnboggs@derryfield.org>
2024-02-15 10:31:48 -0500

    Tally.java example

---
+++ Unit 08/Tally.java
@@ -0,0 +1,9 @@
+class Counter{
+
+}
+public class Tally{
+    public static void main(String[] args) {
+        System.out.println("This is a tally counter");
+        Counter myCounter = new Counter(); //counter of counter type using new operator and counter instructor
+    }
+}
