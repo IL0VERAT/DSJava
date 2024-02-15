53b8011f96c5caa42b38a7ebf84465a3244a87f5
IL0VERAT <26mlinnboggs@derryfield.org>
2024-02-15 10:48:59 -0500

    Update Tally

--- Unit 08/Tally.java
+++ Unit 08/Tally.java
@@ -1,4 +1,24 @@
 class Counter{
+    private int count; //member variable --> can't access it, only stuff inside counter see it
+    
+    //private and public and modifiers
+    public int getValue(){ //implicite argument
+        return count;
+    }
+
+    //Declare method to increase the count on device by 1
+    public void count(){
+        if(count == 9999){
+            count = 0;
+        } else {
+        count++;
+        }
+    }
+
+    //Declare method to reset counter
+    public void reset(){
+        count = 0;
+    }
 
 }
 public class Tally{
