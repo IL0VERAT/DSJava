cb45d14c3d79f59f74237ddc7c578b56fa36de3c
IL0VERAT <26mlinnboggs@derryfield.org>
2024-02-17 15:58:00 -0500

    Update QuizScores

--- Unit 07/Throwing Exceptions/QuizScores.java
+++ Unit 07/Throwing Exceptions/QuizScores.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class QuizScores {
+    public static int getGrade(Scanner in) {
+        try {
+            int score = Integer.valueOf(in.nextInt());
+            return score;
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
+    }
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a test score: ");
+        getGrade(in);
+
         
     }
 }
