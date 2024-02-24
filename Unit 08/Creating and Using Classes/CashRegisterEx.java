32ecfdba436760a22e2f54a11ac36b7e813b4670
IL0VERAT <26mlinnboggs@derryfield.org>
2024-02-24 16:12:34 -0500

    Created CashRegisterEx

---
+++ Unit 08/Creating and Using Classes/CashRegisterEx.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+class CashRegister {
+    private ArrayList<Double> items;
+    /*
+     * Constructor.
+     */
+    public CashRegister() {
+        // put implementation here.
+    }
+ 
+ 
+    /*
+     * Add the price of an item to the register's total.
+     */
+    public void addItem(double price) {
+        // put implementation here.
+    }
+ 
+ 
+    /*
+     * Remove the most recently added item from the current sale.
+     */
+    public void removeItem() {
+        // put implementation here.
+    }
+ 
+ 
+    /*
+     * Return the current total sales amount.
+     */
+    public double getTotal() {
+        // put implementation here.
+    }
+ 
+ 
+    /*
+     * Return the total count of items in the current sale.
+     */
+    public int getCount() {
+        // put implementation here.
+    }
+ 
+ 
+    /*
+     * Clear the register (total and count) for a new sale.
+     */
+    public void clear() {
+        // put implementation here.
+    }
+ }
+ 
+public class CashRegisterEx{
+    public static void main(String[] args) {
+        
+    }
+}
