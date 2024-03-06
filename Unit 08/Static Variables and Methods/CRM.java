a4b13d5acf08e4bd5179217cea9052108f18236c
IL0VERAT <26mlinnboggs@derryfield.org>
2024-03-06 11:16:00 -0500

    Update CRM

--- Unit 08/Static Variables and Methods/CRM.java
+++ Unit 08/Static Variables and Methods/CRM.java
@@ -3,9 +3,7 @@ class customer{
     private int count;
     private String name;
     private int region;
-    /*
-     * Constructor
-     */
+    
     public customer(String new_name, int new_region){
         name = new_name;
         region = new_region;
@@ -46,10 +44,25 @@ class customer{
     public void setRegion(int new_region){
         region = new_region;
     }
+
+    public double getTotalTax(){
+        double rate = getTaxRate(region);
+        return rate * total;
+    }
+
+    public static double getTaxRate(int region){
+        return switch(region){
+            case 1 -> 0;
+            case 2 -> 0.0550;
+            case 3 -> 0.0600;
+            case 4 -> 0.0625;
+            default -> 0;
+        };
+    }
 }
 public class CRM{
     public static void main(String[] args) {
-        customer c1 = new customer("Harvest Market", 1);
+        customer c1 = new customer("Harvest Market", 4);
         customer c2 = new customer("Wegman's", 4);
 
         c1.addSaleAmount(200);
@@ -57,7 +70,7 @@ public class CRM{
 
         c2.addSaleAmount(2000);
         
-        System.out.printf("%-16s: $%6.2f (%03d)%n", c1.getName(), c1.getTotal(), c1.getCount());
+        System.out.printf("%-16s: $%6.2f (%03d) Taxes: $%6.2f %n", c1.getName(), c1.getTotal(), c1.getCount(), c1.getTotalTax());
         System.out.printf("%-16s: $%6.2f (%03d)%n", c2.getName(), c2.getTotal(), c2.getCount());
     }
 }
