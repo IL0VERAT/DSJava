90f80a5b588fc3d09fe6a8f22a65c7f81e97e471
IL0VERAT <26mlinnboggs@derryfield.org>
2024-03-06 11:06:05 -0500

    Created CRM example

---
+++ Unit 08/Static Variables and Methods/CRM.java
@@ -0,0 +1,63 @@
+class customer{
+    private double total;
+    private int count;
+    private String name;
+    private int region;
+    /*
+     * Constructor
+     */
+    public customer(String new_name, int new_region){
+        name = new_name;
+        region = new_region;
+        total = 0;
+        count = 0;
+    }
+
+    public double getTotal(){
+        return total;
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+    public void addSaleAmount(double newAmount){
+        total = total + newAmount;
+        count++;
+    }
+
+    public void clearTotal(){
+        total = 0;
+        count = 0;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String new_name){
+        name = new_name;
+    }
+
+    public int getRegion(){
+        return region;
+    }
+
+    public void setRegion(int new_region){
+        region = new_region;
+    }
+}
+public class CRM{
+    public static void main(String[] args) {
+        customer c1 = new customer("Harvest Market", 1);
+        customer c2 = new customer("Wegman's", 4);
+
+        c1.addSaleAmount(200);
+        c1.addSaleAmount(100);
+
+        c2.addSaleAmount(2000);
+        
+        System.out.printf("%-16s: $%6.2f (%03d)%n", c1.getName(), c1.getTotal(), c1.getCount());
+        System.out.printf("%-16s: $%6.2f (%03d)%n", c2.getName(), c2.getTotal(), c2.getCount());
+    }
+}
