f4f20b7c8555915b6ef319c14b0563ed0fe0670b
IL0VERAT <26mlinnboggs@derryfield.org>
2024-03-07 14:22:23 -0500

    Update CashRegisterEX

--- Unit 08/Creating and Using Classes/CashRegisterEx.java
+++ Unit 08/Creating and Using Classes/CashRegisterEx.java
@@ -1,55 +1,75 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-class CashRegister {
-    //member variables
-    private ArrayList<Double> items;
-    private double cost;
 
-    //constructor
+class CashRegister {
+    // member variables
+    private ArrayList<Double> items = new ArrayList<>();
+    private double cost;
+    private int counter;
+
+    // constructor
     public CashRegister() {
+        counter = 0;
         cost = 0;
     }
- 
-    //adds 1 item to the the 
+
+    // adds 1 item to the the
     public void addItem(double price) {
-        
+        items.add(price);
+        counter++;
     }
- 
- 
+
     /*
      * Remove the most recently added item from the current sale.
      */
     public void removeItem() {
-        // put implementation here.
+        items.remove(counter);
+        counter--;
     }
- 
- 
+
     /*
      * Return the current total sales amount.
      */
     public double getTotal() {
-        // put implementation here.
+        for (int i = 0; i < items.size(); i++) {
+            cost += items.get(i);
+        }
+        return cost;
     }
- 
- 
+
     /*
      * Return the total count of items in the current sale.
      */
     public int getCount() {
-        // put implementation here.
+        return counter;
     }
- 
- 
+
     /*
      * Clear the register (total and count) for a new sale.
      */
     public void clear() {
-        // put implementation here.
+        items.clear();
+        counter = 0;
     }
- }
- 
-public class CashRegisterEx{
+}
+
+public class CashRegisterEx {
     public static void main(String[] args) {
-        
+        CashRegister cashRegister = new CashRegister();
+        Scanner in = new Scanner (System.in);
+        String input;
+        boolean flag = true;
+
+        while(flag == true){
+            System.out.print("Enter a price, or t to show total, c to clear, d to delete most recent input, or q to quit:  ");
+            input = in.next();
+            switch(input.charAt(0)){
+                case 't' -> System.out.println("Total = " + cashRegister.getTotal() + " Count = " + cashRegister.getCount());
+                case 'c' -> cashRegister.clear();
+                case 'd' -> cashRegister.removeItem();
+                case 'q' -> flag = false;
+                default -> cashRegister.addItem(Double.valueOf(input));
+            }
+        }
     }
 }
