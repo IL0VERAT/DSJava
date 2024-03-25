5a75dc648f33774442a2ac901c9340d3dd4d3256
IL0VERAT <26mlinnboggs@derryfield.org>
2024-03-24 21:20:20 -0400

    Update Magic8Ball

--- Unit 09/Unit 09 Programming Project/Magic8BallShowcase.java
+++ Unit 09/Unit 09 Programming Project/Magic8BallShowcase.java
@@ -1,7 +1,19 @@
 import java.util.Scanner;
 import org.derryfield.toys.Magic8Ball;
+
 public class Magic8BallShowcase {
     public static void main(String[] args) {
         Magic8Ball magic8Ball = new Magic8Ball();
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Ask a yes or no question or type q to quit: ");
+            
+            if (in.hasNext("q") || in.hasNext("Q")) {
+                break;
+            } else {
+                Magic8Ball.shake();
+            }
+        } 
     }
 }
---
+++ Unit 09/Unit 09 Programming Project/org/derryfield/toys/Magic8Ball.java
@@ -0,0 +1,38 @@
+package org.derryfield.toys;
+
+public class Magic8Ball {
+    private static String result;
+
+    public Magic8Ball() {
+
+    }
+
+    public static void shake() {
+        int randomNum = (int) (Math.random() * 20);
+        result = switch (randomNum) {
+            case 0 -> "It is certain";
+            case 1 -> "It is decidely so";
+            case 2 -> "Without a doubt";
+            case 3 -> "Yes definitely";
+            case 4 -> "You may rely on it";
+            case 5 -> "As I see it, yes";
+            case 6 -> "Most likely";
+            case 7 -> "Outlook good";
+            case 8 -> "Yes";
+            case 9 -> "Signs point to yes";
+            case 10 -> "Reply hazy, try again";
+            case 11 -> "Ask again later";
+            case 12 -> "Better not tell you now";
+            case 13 -> "Cannot predict now";
+            case 14 -> "Concentrate and ask again";
+            case 15 -> "Don't count on it";
+            case 16 -> "My reply is no";
+            case 17 -> "My sources say no";
+            case 18 -> "Outlook not so good";
+            case 19 -> "Very doubtful";
+            default -> "It is certain";
+        };
+        System.out.print(result);
+    }
+
+}
