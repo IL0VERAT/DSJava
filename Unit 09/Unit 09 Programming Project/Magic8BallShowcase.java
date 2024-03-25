ea8266d999077f7de085b621c5755528c9dff168
IL0VERAT <26mlinnboggs@derryfield.org>
2024-03-24 20:56:46 -0400

    Worked out package format

---
+++ Unit 09/Unit 09 Programming Project/Magic8BallShowcase.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+import org.derryfield.toys.Magic8Ball;
+public class Magic8BallShowcase {
+    public static void main(String[] args) {
+        Magic8Ball magic8Ball = new Magic8Ball();
+    }
+}
