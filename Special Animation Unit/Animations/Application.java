d67d2e36fcee63a978124b0364e3a9dab43eeac0
IL0VERAT <26mlinnboggs@derryfield.org>
2024-04-05 12:59:46 -0400

    Added OS checkOS()

--- Special Animation Unit/Animations/Application.java
+++ Special Animation Unit/Animations/Application.java
@@ -4,9 +4,48 @@ public class Application extends JFrame{
     private final int FRAME_WIDTH = 640;
     private final int FRAME_HEIGHT = 480;
 
+    public static enum OS {
+        WIN, MAC, LINUX, OTHER
+    }
+
+    //checks the type of operating system
+    public static OS checkOS() {
+        OS val;
+        String name = System.getProperty("os.name").toLowerCase();
+        if (name.indexOf("win") >= 0) {
+            val = OS.WIN;
+        } else if (name.indexOf("mac") >= 0) {
+            val = OS.MAC;
+        } else if (name.indexOf("nux") >= 0) {
+            val = OS.LINUX;
+        } else {
+            val = OS.OTHER;
+        }
+
+        return val;
+    }
+
     //constructor
     public Application(String title) {
-        
+        //will need to account for border and bar dimensions
+        int border_width = 0;
+        int bar_height = 0;
+        OS os = checkOS();
+        switch (os) {
+            case WIN:
+                border_width = 7;
+                bar_height = 30;
+                break;
+            case MAC:
+                border_width = 0;
+                bar_height = 28;
+                break;
+            case LINUX:
+                // I don't know what the values are for Linux.
+                break;
+            case OTHER:
+                break;
+        }
         // Use the inherited setTitle() method
         // to set the title for our frame.
         this.setTitle(title);
--- Special Animation Unit/DemoApp.java
+++ Special Animation Unit/DemoApp.java
@@ -1,7 +1,7 @@
 import Animations.Application;
 public class DemoApp {
     public static void main(String[] args) {
-        Application myApp = new Application("TITLE");
+        Application myApp = new Application("MY DEMO");
         myApp.setVisible(true);
     }
 }
