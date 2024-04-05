95b1ad43dfb5b82576106c2a085cfe4412429262
IL0VERAT <26mlinnboggs@derryfield.org>
2024-04-04 21:09:03 -0400

    Added comments to Ride.java

--- Unit 09/Lesson 29/Ride.java
+++ Unit 09/Lesson 29/Ride.java
@@ -1,10 +1,18 @@
+//Coder: Milo Linn-Boggs Date: 04 April 2024
 public class Ride {
     public static void main(String[] args) {
-        Bicycle b = new Bicycle();
-        b.pedal();
-        b.pedal();
 
+        //example of parent class bicycle
+        Bicycle b = new Bicycle();
+        System.out.println("Standard Bicycle");
+        b.pedal();
+        b.pedal();
+        b.display();
+        System.out.println("");
+
+        //example of geared bicycle class with overridden breaking method 
         GearedBicycle gb = new GearedBicycle();
+        System.out.println("Geared Bicycle:");
         gb.pedal();
         gb.display();
         gb.shiftUp();
