98cde9381d6e2aae479a99b6d5bda31b751c3c2a
IL0VERAT <26mlinnboggs@derryfield.org>
2024-04-09 10:07:18 -0400

    AUpdate

--- Special Animation Unit/DemoApp.java
+++
@@ -1,7 +0,0 @@
-import Animations.Application;
-public class DemoApp {
-    public static void main(String[] args) {
-        Application myApp = new Application("MY DEMO");
-        myApp.setVisible(true);
-    }
-}
