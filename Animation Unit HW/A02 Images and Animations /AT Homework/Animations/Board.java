032a92d8f69bfedcb6b1b1b059fb062d34555aeb
IL0VERAT <26mlinnboggs@derryfield.org>
2024-04-19 10:34:55 -0400

    Update Board

--- Animation Unit HW/A02 Images and Animations /AT Homework/Animations/Board.java
+++ Animation Unit HW/A02 Images and Animations /AT Homework/Animations/Board.java
@@ -1,72 +1,85 @@
+//Coder: Milo Linn-Boggs Date: 19 April 2024
 package Animations;
 
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Shape;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.Ellipse2D;
 import java.io.File;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
+//private member variable
 public class Board extends JPanel {
     private final int B_WIDTH = 720;
     private final int B_HEIGHT = 720;
     private int x = 0;
     private int y = 0;
-    private final int DIAMETER = 20;
     private Timer timer;
     private final int INITAL_DELAY = 100;
     private final int PERIOD_DELAY = 25;
     private int xSpeed = 2;
+    private int ySpeed = 2;
+    private int angle = 0;
     private BufferedImage img;
 
     // constructor
     public Board() {
         setBackground(Color.CYAN);
         setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
-        x = B_WIDTH/2;
-        y = B_HEIGHT/2;
+        x = B_WIDTH / 2;
+        y = B_HEIGHT / 2;
 
         timer = new Timer();
-        timer.scheduleAtFixedRate(new ScheduledUpdate(),INITAL_DELAY,PERIOD_DELAY);
+        timer.scheduleAtFixedRate(new ScheduledUpdate(), INITAL_DELAY, PERIOD_DELAY);
 
+        //imports the photo
         try {
-            File imageFile = new File("Media/Andy.jpg");
+            File imageFile = new File("Media/Andy.png");
             img = ImageIO.read(imageFile);
-            setPreferredSize(new Dimension(img.getWidth(), img.getHeight()));
-        } catch (Exception fileNotException){
+        } catch (Exception fileNotException) {
             System.err.println(fileNotException.getMessage());
         }
+
+        //randomly assigns the speed
+        xSpeed = (int) (Math.random() * 5 + 1);
+        ySpeed = (int) (Math.random() * 5 + 1);
     }
 
-    private class ScheduledUpdate extends TimerTask{
+    //moves the photo
+    private class ScheduledUpdate extends TimerTask {
         public void run() {
             x += xSpeed;
             if (x > B_WIDTH) {
                 x = 0;
             }
+            y += ySpeed;
+            if (y > B_HEIGHT) {
+                y = 0;
+            }
+            angle += 5; //in degrees
+            angle = angle%360;
+
             repaint();
         }
     }
 
-    public void paintComponent(Graphics g){
+    //paints the photo onto window
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
         AffineTransform affineTransform = new AffineTransform();
-        affineTransform.translate(x - DIAMETER / 2, y - DIAMETER / 2);
-
-        Ellipse2D ellipse = new Ellipse2D.Double(0,0,DIAMETER,DIAMETER);
-        Shape transformedShape = affineTransform.createTransformedShape(ellipse);
-        g2d.setColor(Color.MAGENTA);
-        g2d.fill(transformedShape);
-
+        if (img != null) {
+            affineTransform.translate(x - img.getWidth() / 2, y - img.getHeight() / 2);
+            //rotates image
+            affineTransform.rotate(Math.toRadians(angle),img.getWidth() / 2, img.getHeight() / 2);
+            g2d.drawImage(img, affineTransform, null);
+        } else {
+            g2d.drawString("Image is null", 100, 100);
+        }
     }
 }
-
