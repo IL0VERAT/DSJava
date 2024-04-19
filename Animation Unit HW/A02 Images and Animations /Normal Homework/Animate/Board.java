8322ba8d7eb5a1cc28cd02dbed2b31e1136ead7b
IL0VERAT <26mlinnboggs@derryfield.org>
2024-04-19 10:55:02 -0400

    Update

--- Animation Unit HW/A02 Images and Animations /Normal Homework/Animate/Board.java
+++ Animation Unit HW/A02 Images and Animations /Normal Homework/Animate/Board.java
@@ -1,11 +1,10 @@
+//Coder: Milo Linn-Boggs Date: 19 April 2024
 package Animate;
 
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Rectangle;
-import java.awt.Shape;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -14,8 +13,8 @@ import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class Board extends JPanel {
-    private final int B_WIDTH = 620;
-    private final int B_HEIGHT = 620;
+    private final int B_WIDTH = 500;
+    private final int B_HEIGHT = 300;
     private BufferedImage img;
 
     // constructor
@@ -23,28 +22,25 @@ public class Board extends JPanel {
         setBackground(Color.CYAN);
         setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
 
+        //imports image
         try {
             File imageFile = new File("Images/dolomites.jpg");
             img = ImageIO.read(imageFile);
-            setPreferredSize(new Dimension(img.getWidth(), img.getHeight()));
+            setPreferredSize(new Dimension(img.getWidth(),img.getHeight()));
         } catch (Exception fileNotException){
             System.err.println(fileNotException.getMessage());
         }
     }
 
+    //paints the photo
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
         AffineTransform affineTransform = new AffineTransform();
-        int x_t = 0, y_t = 0;
-        int x_scaled = 0, y_scaled = 0;
-        double scale = 0.25;
+        double scale = 0.5;
         if(img != null){
-            x_t = this.getWidth() / 2;
-            x_scaled = (int) ((img.getWidth() * scale) / 2.0);
-            y_t = this.getHeight() / 2;
-            y_scaled = (int) ((img.getHeight() * scale) / 2.0);
-            affineTransform.translate(x_t + x_scaled, y_t + y_scaled);
+            //moves photo
+            affineTransform.translate(getWidth()/2, getHeight()/2);
             affineTransform.scale(scale, scale);
             g2d.drawImage(img, affineTransform, null);
         } else {
