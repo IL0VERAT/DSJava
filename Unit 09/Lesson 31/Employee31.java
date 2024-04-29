93c0bfdd10ed41b2fc9f9d5a343a61dcc3c3def7
IL0VERAT <26mlinnboggs@derryfield.org>
2024-04-29 10:27:23 -0400

    Classwork

---
+++ Unit 09/Lesson 31/Employee31.java
@@ -0,0 +1,47 @@
+public class Employee31 {
+    private String name;
+    private double salary;
+
+    /*
+     * Constructor.
+     */
+    public Employee31(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double newSalary) {
+        salary = newSalary;
+    }
+
+    public double getAnnualIncome() {
+        // Employees only get an annual salary.
+        return getSalary();
+    }
+
+    public void displayInfo() {
+        System.out.println(name + ":");
+        System.out.printf("  Salary = $%,12.2f%n", salary);
+    }
+
+    //overide toString() method
+    public String toString(){
+        String val = this.getClass().getName() 
+        + "[name = ]" + this.name 
+        + ", salary = " + this.salary + "]";
+        return val;
+
+    }
+}
---
+++ Unit 09/Lesson 31/ToStringDemo.java
@@ -0,0 +1,11 @@
+import java.awt.Rectangle;
+
+public class ToStringDemo{
+    public static void main(String[] args) {
+        Rectangle box = new Rectangle (5,10,20,30);
+        //System.out.print(box);
+
+        Employee31 ed = new Employee31("Gerald", 10000000);
+        System.out.println(ed);
+    }
+}
