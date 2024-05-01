7746b90b7a15b2d3559b2478b9d90f7287592527
IL0VERAT <26mlinnboggs@derryfield.org>
2024-05-01 10:56:16 -0400

    Update Board.java

--- Final Project/animate/Board.java
+++ Final Project/animate/Board.java
@@ -11,44 +11,42 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.geom.AffineTransform;
 
-//private member variable
+//private member variables
 public class Board extends JPanel implements KeyListener{
-    private final int B_WIDTH = 160;
-    private final int B_HEIGHT = 90;
+    private final int B_WIDTH = 1600;
+    private final int B_HEIGHT = 900;
     private final int B_FLOOR = B_HEIGHT - 25;
 
     // constructor
     public Board() {
         setBackground(Color.CYAN);
         setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+
+        //needs to be able to get focus of user interface
         this.setFocusable(true);
+        //register ourselves as a keylistener object
         this.addKeyListener(this);
     }
 
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_SPACE) {
             System.out.println("Spacebar was pressed.");
-        } 
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
             System.out.println("Left arrow was pressed.");
-        } 
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
             System.out.println("Right arrow was pressed.");
-        } 
-        if (e.getKeyCode() == KeyEvent.VK_UP) {
+        } else if (e.getKeyCode() == KeyEvent.VK_UP) {
             System.out.println("Up arrow was pressed.");
-        } 
-        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-            System.out.println("Left arrow was pressed.");
-        } 
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            System.out.println("Down arrow was pressed.");
+        } else {
+        }
     }
 
     public void keyReleased(KeyEvent e) {
-        //throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
     }
 
-    public void keyTyped(KeyEvent e) {
-        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+    public void keyTyped(KeyEvent e) {  
     }
 
     //paints the photo onto window
