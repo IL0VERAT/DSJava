837fe738a530aaeac0dfc897025ea31fb4e43e7a
IL0VERAT <26mlinnboggs@derryfield.org>
2024-05-01 23:24:36 -0400

    Added comments

--- Unit 09/Lesson 31/BaseballCardDemo.java
+++ Unit 09/Lesson 31/BaseballCardDemo.java
@@ -1,11 +1,15 @@
 public class BaseballCardDemo {
     public static void main(String[] args) {
+
+        //creates BaseBallCard objects
         BaseballCard ruth = new BaseballCard("Babe Ruth", 1914);
         BaseballCard mays = new BaseballCard("Willie Mays", 1972);
 
+        //prints out information
         System.out.println(ruth);
         System.out.println(mays);
 
+        //compares the two cards
         if(ruth.equals(mays)){
             System.out.println("We have duplicates!");
         } else {
